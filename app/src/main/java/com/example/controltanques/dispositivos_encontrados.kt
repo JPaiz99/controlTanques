857d2761521ffdf1749a3d4d7b365dfa80349c1b
Arceus91 <jpaizb4@miumg.edu.gt>
2022-06-25 12:49:00 -0600

    activacion bluetooth

--- app/src/main/java/com/example/controltanques/dispositivos_encontrados.kt
+++ app/src/main/java/com/example/controltanques/dispositivos_encontrados.kt
@@ -1,11 +1,42 @@
 package com.example.controltanques
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_dispositivos_encontrados.*
 
 class dispositivos_encontrados : AppCompatActivity() {
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dispositivos_encontrados)
+
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+
+        if (bluetoothAdapter?.isEnabled ==false){
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE).apply {
+                startActivity(this)
+            }
+
+        }
+        bt.setOnClickListener(){
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
+            pairedDevices?.forEach { device ->
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+            }
+
+
+        }
+
+
+
     }
 }
