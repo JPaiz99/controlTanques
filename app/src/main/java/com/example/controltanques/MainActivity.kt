0f629a3528ff96f82e7e670fef26f3d30843844a
Arceus91 <jpaizb4@miumg.edu.gt>
2022-06-25 11:28:46 -0600

    Movimiento entre layouts

--- app/src/main/java/com/example/controltanques/MainActivity.kt
+++ app/src/main/java/com/example/controltanques/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.controltanques
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        escanear.setOnClickListener(){
+            val intent:Intent = Intent(this,dispositivos_encontrados::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/controltanques/dispositivos_encontrados.kt
@@ -0,0 +1,11 @@
+package com.example.controltanques
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dispositivos_encontrados : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dispositivos_encontrados)
+    }
+}
